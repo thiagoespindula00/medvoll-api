7afb4ebf822dd5187ab8a3e3b929851ae2c5806c
= <thiagoespindula00@gmail.com>
2024-11-24 18:16:02 -0300

    Criada uma classe para teste automatizado do MedicoRepository. Dupliquei o aplication.properties ajustando o nome para "aplication-test.properties" e na classe de teste incluí a anotação ActiveProfiles("test") indicando o arquivo das propriedades que o teste deve buscar ao ser executado

--- src/main/java/com/example/MedVollAPI/security/SecurityConfigurations.java
+++ src/main/java/com/example/MedVollAPI/security/SecurityConfigurations.java
@@ -25,6 +25,9 @@ public class SecurityConfigurations {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req -> {
                     req.requestMatchers("/login").permitAll();
+                    //req.requestMatchers(("/context-path/swagger-ui.html")).permitAll();
+                    //req.requestMatchers(("/context-path/v3/api-docs")).permitAll();
+                    //req.requestMatchers(("/swagger-ui/**")).permitAll();
                     req.anyRequest().authenticated();
                 })
                 // Dizendo pro Spring para primeiro passar pelo filtro que eu criei para depois ele passar pelo filtro do próprio Spring
---
+++ src/test/java/com/example/MedVollAPI/repository/MedicoRepositoryTest.java
@@ -0,0 +1,17 @@
+package com.example.MedVollAPI.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class MedicoRepositoryTest {
+
+    @Test
+    void escolheMedicoAleatorioNaData() {
+
+    }
+}
