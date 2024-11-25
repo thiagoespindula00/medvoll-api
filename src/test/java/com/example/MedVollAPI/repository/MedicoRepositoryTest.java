b092b8c28443365c04ce19912798955859151101
= <thiagoespindula00@gmail.com>
2024-11-25 18:35:11 -0300

    Feito um teste simples. Para build do projeto: O Maven quem fará, basta rodar a opção "package" e será gerado um arquivo .jar dentro da pasta "target" do projeto. Para subir a aplicação abrir o terminal e executar o comando java -jar "nome-do-arquivo-gerado.jar". Lembrar de configurar as variáveis de ambiente

--- src/test/java/com/example/MedVollAPI/repository/MedicoRepositoryTest.java
+++ src/test/java/com/example/MedVollAPI/repository/MedicoRepositoryTest.java
@@ -1,17 +1,34 @@
 package com.example.MedVollAPI.repository;
 
+import com.example.MedVollAPI.model.Especialidade;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 class MedicoRepositoryTest {
 
-    @Test
-    void escolheMedicoAleatorioNaData() {
+    @Autowired
+    private MedicoRepository medicoRepository;
 
+    @Test
+    @DisplayName("Deveria devolver null quando unico medico cadastrado nao esta disponvel na data")
+    void escolheMedicoAleatorioNaDataCenario1() {
+        var proximaSegundaAs10 = LocalDate.now()
+                .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+                .atTime(10, 0);
+        var medicoLivre = medicoRepository.escolheMedicoAleatorioNaData(Especialidade.CARDIOLOGIA, proximaSegundaAs10);
+        assertThat(medicoLivre).isNull();
     }
 }
